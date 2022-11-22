d9e1a5410d8521e66ecdf952ff2f295b7164ec02
AndreaCaro <79027755+AndreaCaroliana@users.noreply.github.com>
2022-11-21 20:00:45 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,63 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import React from 'react';
+import {} from 'lodash';
+
+
+import { LoginScreen } from './src/screens/LoginScreen';
+import { MangaScreen } from './src/screens/MangaScreen';
+import NewChapterScreen from './src/screens/NewChapterScreen';
+import ReaderScreen from './src/screens/ReaderScreen';
+import { RegisterScreen } from './src/screens/RegisterScreen';
+import SearchScreen from './src/screens/SearchScreen';
+
+export type RootStackParamList = {
+  manga: { id?: number };
+  newChapter: { manga_id: number };
+  search?: {
+    only_creator?: boolean;
+    only_subscription?: boolean;
+  };
+  reader: {
+    chapter_id: number;
+    page?: number;
+    direction?: string;
+  };
+};
+
+const MangaReader = createNativeStackNavigator<RootStackParamList>();
+
+const MangaReaderNavigator = () => {
+  return (
+    <MangaReader.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <MangaReader.Screen name="search" component={SearchScreen} />
+      <MangaReader.Screen name="manga" component={MangaScreen} />
+      <MangaReader.Screen name="reader" component={ReaderScreen} />
+      <MangaReader.Screen name="newChapter" component={NewChapterScreen} />
+    </MangaReader.Navigator>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="home" component={MangaReaderNavigator} />
+        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ config.ts
@@ -0,0 +1,12 @@
+export default {
+  server: "https://manga-backend-reader.herokuapp.com",
+  firebase: {
+    apiKey: "AIzaSyCVhLbDHOsYEVRoQb5GStg0l4UG0E1RHPE",
+    authDomain: "manga-react-reader.firebaseapp.com",
+    projectId: "manga-react-reader",
+    storageBucket: "manga-react-reader.appspot.com",
+    messagingSenderId: "712346601170",
+    appId: "1:712346601170:web:f8ee8adb9e3cdfb88a8f0b",
+    measurementId: "G-XF7XJ2HMB5",
+  },
+};
